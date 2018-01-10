b685297d79aecd37a75efc4c310f61bdff5455b2
Tom Janssens <tom@corebvba.be>
2018-01-10 14:33:30 +0100

    Added vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <template>
   <div id="app">
     <h1>Vuexel</h1>
-    <sheet></sheet>
+    <router-link to="/home">All sheets</router-link>
+    <router-link to="/sheets/new">New sheet</router-link>
+    <router-view>
+    </router-view>
   </div>
 </template>
 
 <script>
+import Vue from "vue";
 import Sheet from "./Sheet.vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+var Home = new Vue({
+  template:"<h1>Home</h1>"
+});
+
+var router = new VueRouter({
+  routes: [
+    {path:"/home", component: Home },
+    {path:"/sheets/new", component: Sheet}
+  ]
+})
 
 export default {
   name: 'app',
+  router,
   components: {
     sheet: Sheet
   }
