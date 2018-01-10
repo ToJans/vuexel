ace876b8c183ecb355ca10233195e3f717fc0c05
Tom Janssens <tom@corebvba.be>
2018-01-10 11:02:04 +0100

    Added evaluation of cells

--- src/Sheet.vue
+++ src/Sheet.vue
@@ -4,7 +4,7 @@
             <tr>
                 <td>{{currentCell.label}}</td>
                 <td :colspan="columnCount-1" class="formula-editor">
-                    <input v-model="currentCellValue" />
+                    <input ref="formula" v-model="gridFormulas[currentCell.label]" />
                 </td>
             </tr>
             <tr>
@@ -20,7 +20,7 @@
                     {{rowLabel(ir)}}
                 </th>
                 <td v-for="ic in columnCount" :key="'c'+ic+'r'+ir" class="cell" 
-                    @click="currentCellRange={row:ir,column:ic}" 
+                    @click="setCurrentCell(ir,ic)" 
                     :class="{'selected-cell':currentCell.row===ir && currentCell.column === ic}">
                     {{cellDisplayValue(ir,ic)}}
                 </td>
@@ -31,13 +31,13 @@
 </template>
 
 <script>
+
 export default {
-  data: function()
-  {
+  data: function()  {
       var rowCount=10,columnCount=26;
-      var gridValues = {B3:123, C5:"Zorro"};
+      var gridFormulas = {B3:"123", C5:"'Zorro"};
       return {
-          rowCount,columnCount,gridValues, currentCellRange: {row:1,column:1}
+          rowCount,columnCount,gridFormulas, currentCellRange: {row:1,column:1}
       }
   },
   methods: {
@@ -73,24 +73,69 @@ export default {
           } else {
               return val;
           }
+      },
+      setCurrentCell: function(row,column) {
+          this.currentCellRange = {row,column};
+          this.$refs.formula.focus();
       }
   }, computed: {
-      currentCell: function () {
-          let row = this.currentCellRange.row;
-          let column = this.currentCellRange.column;
-          let label = this.cellLabel(row,column);
-          return {
-            row,column,label
-          }
+      currentCell:  function () {
+            let row = this.currentCellRange.row;
+            let column = this.currentCellRange.column;
+            let label = this.cellLabel(row,column);
+            return {
+                row,column,label
+            }
       },
       currentCellValue: {
         get:function () {
-            return this.currentCell.value;
+            return this.cellValue(this.currentCell.row,this.currentCell.column);
         },
-        set: function (value) {
-            this.$set(this.gridValues, this.currentCell.label,value);
+        set: function (formula) {
+            this.$set(this.gridFormulas, this.currentCell.label,formula);
         }
+      },
+      gridValues: function () {
+        let cached  = {};
+        let scope = {};
+        var labels = Object.keys(this.gridFormulas);
+        for (const l in this.gridFormulas) {
+            const label = l;
+            const fmla = this.gridFormulas[label];
+            Object.defineProperty(scope,label,{
+                get: function(){
+                    let retval = "#ERROR";
+                    try {
+                        if (cached.hasOwnProperty(label)) {
+                            retval = cached[label];
+                        } else if (fmla === null) {
+                            retval = null;
+                        } else if (fmla[0] === "'") {
+                            retval = fmla.substring(1);
+                        } else if (fmla[0] === "=") {
+                            const fnText = `let res;with(__scope){ res=(${fmla.substring(1)});}; return res;`
+                            var fn = new Function("__scope",fnText);
+                            retval = fn(scope);
+                        } else {
+                            retval = parseFloat(fmla);
+                        }
+                    } catch (e) {
+                        retval="#ERROR "+e;
+                    }
+                    cached[label] = retval;
+                    return retval
+                },
+                enumerable: false,
+                configurable: false
+            })
+        }
+        var retval = {};
+        for (var k in this.gridFormulas) {
+            retval[k] = scope[k];
+        }
+        return retval;
       }
+
   }
 }
 </script>
